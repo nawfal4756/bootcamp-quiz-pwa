b6c784c1edd5c752c44a678d8509c717c830dc85
nawfal4756 <nawfal4756@users.noreply.github.com>
2020-08-24 19:00:35 +0500

    SW Updated

--- public/sw.js
+++ public/sw.js
@@ -36,23 +36,29 @@ var urlsToCache = [
 self.addEventListener('install', function(event) {
 	// Perform install steps
 	event.waitUntil(
-		caches.open(CACHE_NAME).then(function(cache) {
-			console.log('Opened cache');
-			return cache.addAll(urlsToCache);
-		})
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			})
+			.catch((err) => console.log('err', err))
 	);
 });
 
 self.addEventListener('fetch', function(event) {
 	if (!navigator.onLine) {
 		event.respondWith(
-			caches.match(event.request).then(function(response) {
-				// Cache hit - return response
-				if (response) {
-					return response;
-				}
-				return fetch(event.request);
-			})
+			caches
+				.match(event.request)
+				.then(function(response) {
+					// Cache hit - return response
+					if (response) {
+						return response;
+					}
+					return fetch(event.request);
+				})
+				.catch((err) => console.log('err', err))
 		);
 	}
 });
--- src/swDev.js
+++ src/swDev.js
@@ -1,6 +1,9 @@
 export default function swDev() {
 	let swDev = `${process.env.PUBLIC_URL}/sw.js`;
-	navigator.serviceWorker.register(swDev).then((result) => {
-		console.log('result', result);
-	});
+	navigator.serviceWorker
+		.register(swDev)
+		.then((result) => {
+			console.log('result', result);
+		})
+		.catch((err) => console.log('err', err));
 }
