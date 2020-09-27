77a40fd7f95abd2fbecb3e1f2002dcec3d1721ce
nawfal4756 <nawfal4756@users.noreply.github.com>
2020-09-27 16:18:16 +0500

    Service Worker Fixed

--- src/swDev.js
+++
@@ -1,7 +0,0 @@
-export default function swDev() {
-	let swDev = `${process.env.PUBLIC_URL}/sw.js`;
-	navigator.serviceWorker
-		.register(swDev)
-		.then()
-		.catch((err) => console.log('err', err));
-}
